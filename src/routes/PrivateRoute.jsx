d319380839826d8b0cbeb7c69552ec51fae71183
redel-byte <alhabibridouane@gmail.com>
2026-04-09 16:16:49 +0100

    feat: creating the current routes in the project and separate them between those files:

---
+++ src/routes/PrivateRoute.jsx
@@ -0,0 +1,42 @@
+import { Routes, Route } from 'react-router-dom'
+import Navbar from '../components/layout/Navbar'
+import Footer from '../components/layout/Footer'
+import Profile from '../pages/private/Profile'
+import Settings from '../pages/private/Settings'
+import MyQuizzes from '../pages/private/MyQuizzes'
+
+const PrivateRoute = ({ children }) => {
+  // This should contain authentication logic
+  // For now, just return children as is
+  return children
+}
+
+const PrivateRoutes = () => {
+  return (
+    <Routes>
+      <Route path="profile" element={
+        <PrivateRoute>
+          <Navbar />
+          <Profile />
+          <Footer />
+        </PrivateRoute>
+      } />
+      <Route path="settings" element={
+        <PrivateRoute>
+          <Navbar />
+          <Settings />
+          <Footer />
+        </PrivateRoute>
+      } />
+      <Route path="my-quizzes" element={
+        <PrivateRoute>
+          <Navbar />
+          <MyQuizzes />
+          <Footer />
+        </PrivateRoute>
+      } />
+    </Routes>
+  )
+}
+
+export default PrivateRoutes
